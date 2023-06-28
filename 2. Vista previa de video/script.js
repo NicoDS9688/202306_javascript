ba1238dcc25d5fa6ef73fca8ed50b23b3ba7c267
NicoDS9688 <nicolasdickel@hotmail.com>
2023-06-28 13:48:03 -0400

    first commit

---
+++ 2. Vista previa de video/script.js
@@ -0,0 +1,12 @@
+console.log("page loaded...");
+
+
+function videoPreview() {
+    var video = document.querySelector("#video");
+    video.play();
+}
+
+function videoPause() {
+    var video = document.querySelector("#video");
+    video.pause();
+}
